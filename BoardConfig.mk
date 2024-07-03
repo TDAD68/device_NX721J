88799a0460398328f0f0fb992058fd3fbbbe646f
TDAD68 <balk.vba0116@gmail.com>
2024-07-04 02:02:27 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/nubia/nx721j
+DEVICE_PATH := device/nubia/NX721J
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
